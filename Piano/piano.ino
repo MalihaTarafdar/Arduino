20d246c5324b3dda3bc3081c1bddecd39ba2476c
Maliha Tarafdar <29731754+MalihaTarafdar@users.noreply.github.com>
2019-11-24 18:49:43 -0500

    Add Piano program

---
+++ Piano/piano.ino
@@ -0,0 +1,54 @@
+/*
+ * This program uses a passive buzzer and 9 buttons to make a piano.
+ * Notes are C, D, E, F, G, A, B, in that order. Octaves are 4-7. 
+ * Octaves can be increased and decreased. 
+ * Author: Maliha Tarafdar
+ */
+
+const int SPEAKER = 12;
+const int BUTTONS[] = {10, 9, 8, 7, 6, 5, 4};
+const int HIGHER_OCTAVE_BUTTON = 2;
+const int LOWER_OCTAVE_BUTTON = 3;
+
+//frequencies of notes in 4th octave
+//Note order: C, D, E, F, G, A, B
+const double notes[] = {261.63, 293.66, 329.63, 349.23, 392, 440, 493.88};
+
+int octave = 4;
+const int OCTAVE_MIN = 4;
+const int OCTAVE_MAX = 7;
+boolean octaveIncreased = false;
+boolean octaveDecreased = false;
+
+void setup() {
+  pinMode(SPEAKER, OUTPUT);
+  for (int i = 0; i < (sizeof(BUTTONS) / sizeof(BUTTONS[0])); i++) {
+    pinMode(BUTTONS[i], OUTPUT);
+  }
+  pinMode(HIGHER_OCTAVE_BUTTON, OUTPUT);
+  pinMode(LOWER_OCTAVE_BUTTON, OUTPUT);
+  Serial.begin(9600);
+}
+
+void playSound(int buttonNum) {
+  while (digitalRead(BUTTONS[buttonNum])) {
+    tone(SPEAKER, notes[buttonNum] * pow(2, octave - OCTAVE_MIN), 100);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < (sizeof(BUTTONS) / sizeof(BUTTONS[0])); i++) {
+    playSound(i);
+  }
+
+  //checks if button to change octave is pressed and checks if the octave was already changed
+  if (digitalRead(HIGHER_OCTAVE_BUTTON) && !octaveIncreased) {
+    octave = constrain(octave + 1, 4, 7);
+    octaveIncreased = true;
+  } else if (digitalRead(LOWER_OCTAVE_BUTTON) && !octaveDecreased) {
+    octave = constrain(octave - 1, 4, 7);
+    octaveDecreased = true;
+  }
+  if (!digitalRead(HIGHER_OCTAVE_BUTTON)) octaveIncreased = false;
+  if (!digitalRead(LOWER_OCTAVE_BUTTON)) octaveDecreased = false;
+}
